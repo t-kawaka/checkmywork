e9a84d9f931091ddd2daed810324a0ad595e6b23
t-kawaka <t.kawaka1136@gmail.com>
2019-04-17 12:08:03 +0000

    FactoryBot.createの表記を修正

--- spec/features/task.spec.rb
+++ spec/features/task.spec.rb
@@ -4,7 +4,7 @@ RSpec.feature "タスク管理機能", type: :feature do
   background do
     FactoryBot.create(:task, id: 1, name:"test_task_01")
     FactoryBot.create(:task, id: 2, name:"test_task_02")
-    FactoryBot.create(:task, id: 3, name:"test_task_03")
+    FactoryBot.create(:second_task, id: 3, name:"test_task_03")
   end
 
   scenario "タスクが作成日時の降順に並んでいるかのテスト" do
