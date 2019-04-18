f0c2c8121b80f3a3f63e949ca2e671781a5f911b
t-kawaka <t.kawaka1136@gmail.com>
2019-04-18 17:02:14 +0000

    index修正（tbody）追加

--- spec/features/task.spec.rb
+++ spec/features/task.spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.feature "タスク管理機能", type: :feature do
   background do
-    FactoryBot.create(:task, id: 1, name:"test_task_01")
-    FactoryBot.create(:task, id: 2, name:"test_task_02")
-    FactoryBot.create(:second_task, id: 3, name:"test_task_03")
+    FactoryBot.create(:task, name:"test_task_01")
+    FactoryBot.create(:task, name:"test_task_02")
+    FactoryBot.create(:second_task, name:"test_task_03")
   end
 
   scenario "タスクが作成日時の降順に並んでいるかのテスト" do
     visit tasks_path
-    expect(Task.order(created_at: "DESC").map(&:id)).to eq [3,2,1]
+    expect(first('tr')).to have_content 'samplesample'
   end
 end
