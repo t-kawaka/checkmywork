6eaa4cb5d1ff0261faf5c659f7c77b7c98dbba71
t-kawaka <t.kawaka1136@gmail.com>
2019-04-19 17:25:46 +0000

    モデル用のテストファイル作成

---
+++ spec/features/task_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.feature "タスク管理機能", type: :feature do
+  background do
+    FactoryBot.create(:task, name:"test_task_01")
+    FactoryBot.create(:task, name:"test_task_02")
+    FactoryBot.create(:second_task, name:"test_task_03")
+  end
+
+  scenario "タスクが作成日時の降順に並んでいるかのテスト" do
+    visit tasks_path
+    expect(first("table")).to have_content 'samplesample'
+  end
+end
