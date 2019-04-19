1a8835bc855d569c498293c065d0658cfe71a80a
t-kawaka <t.kawaka1136@gmail.com>
2019-04-19 15:52:33 +0000

    nameカラム修正

---
+++ db/migrate/20190419154537_change_column_to_task.rb
@@ -0,0 +1,9 @@
+class ChangeColumnToTask < ActiveRecord::Migration[5.2]
+  def up
+    change_column :tasks, :name, :string, null: false, limit: 30
+  end
+
+  def down
+    change_column :tasks, :name, :string, null: false
+  end
+end
