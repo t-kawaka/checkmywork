02d40769430478417819d49678d0c7942540cadb
t-kawaka <t.kawaka1136@gmail.com>
2019-04-20 07:57:24 +0000

    バリデーションで明日以降に設定しないとエラーが発生するよう対応

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,12 @@
 class Task < ApplicationRecord
   validates :name, presence: true, length: { maximum: 30 }
   validates :detail, presence: true
+  validates :deadline, presence: true
+  validate :deadline_cannnot_be_in_the_past_and_today
+
+  def deadline_cannnot_be_in_the_past_and_today
+    if deadline.present? && deadline.past?
+      errors.add(:deadline, "は、明日以降に設定してください！")
+    end
+  end
 end
