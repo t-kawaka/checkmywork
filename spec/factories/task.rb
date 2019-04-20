262a35abf0d8217c4b1bf210204b1868b86ee372
t-kawaka <t.kawaka1136@gmail.com>
2019-04-20 17:30:17 +0000

    FeatureSpecの各テストコード作成

--- spec/factories/task.rb
+++ spec/factories/task.rb
@@ -2,10 +2,12 @@ FactoryBot.define do
   factory :task do
     name { 'test_task_01'}
     detail { 'testtesttest' }
+    deadline { DateTime.now + 1 }
   end
 
   factory :second_task, class: Task do
     name { 'test_task_03'}
     detail { 'samplesample' }
+    deadline { DateTime.now + 1}
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,17 +3,22 @@ require 'rails_helper'
 RSpec.describe Task, type: :model do
 
   it "titleが空ならバリデーションが通らない" do
-    task = Task.new(name: '', detail: '失敗テスト')
+    task = Task.new(name: '', detail: '失敗テスト', deadline: DateTime.now + 1)
     expect(task).not_to be_valid
   end
 
   it "contentが空ならバリデーションが通らない" do
-    task = Task.new(name: "失敗テスト", detail: "")
+    task = Task.new(name: "失敗テスト", detail: "", deadline: DateTime.now + 1)
     expect(task).not_to be_valid
   end
 
-  it "titleとcontentに内容が記載されていればバリデーションが通る" do
-    task = Task.new(name:"成功テスト", detail: "成功テスト")
+  it "deadlineが空ならバリデーションが通らない" do
+    task = Task.new(name: "失敗テスト", detail: "失敗テスト", deadline: nil)
+    expect(task).not_to be_valid
+  end
+
+  it "titleとcontentとdeadlineに内容が記載されていればバリデーションが通る" do
+    task = Task.new(name:"成功テスト", detail: "成功テスト", deadline: DateTime.now + 1)
     expect(task).to be_valid
   end
 end
