2763274f0a19e13e686ff0075ecf83f489fcedd8
t-kawaka <t.kawaka1136@gmail.com>
2019-04-29 14:43:47 +0000

    UserのModel作成・セキュアパスワード対応

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_23_131611) do
+ActiveRecord::Schema.define(version: 2019_04_29_141526) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(version: 2019_04_23_131611) do
     t.integer "priority", default: 0, null: false
     t.index ["priority"], name: "index_tasks_on_priority"
     t.index ["situation"], name: "index_tasks_on_situation"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
