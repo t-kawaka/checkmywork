c09d88eac72a7e8d59871b1c2bdc6e1c74cf5a1b
t-kawaka <t.kawaka1136@gmail.com>
2019-04-30 05:28:51 +0000

    ログイン機能実装（アソシエーション未実装）

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,31 @@
+class SessionsController < ApplicationController
+  skip_before_action :login_required, only: [:new, :create]
+  before_action :correct_user,  only: [:show]
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: session_params[:email].downcase)
+    if user && user.authenticate(session_params[:password])
+      session[:user_id] = user.id
+      flash[:notice] = 'ログインに成功しました'
+      redirect_to root_path
+    else
+      flash.now[:danger] = 'ログインに失敗しました'
+      render 'new'
+    end
+  end
+
+  def destroy
+    reset_session
+    flash[:notice] = 'ログアウトしました'
+    redirect_to root_path
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:email, :password)
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,50 +1,38 @@
 class UsersController < ApplicationController
-  def index
-    @users = User.all
-  end
+  skip_before_action :login_required, only: [:new, :create]
+  before_action :correct_user, only: [:show]
 
   def show
-    @user = User.find(params[:id])
   end
 
   def new
-    @user = User.new
+    if logged_in?
+      redirect_to root_path
+    else
+      @user = User.new
+    end
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:notice] = "タスク「#{@user.user}」を作成しました"
-      redirect_to user_path(@user.id)
+      session[:user_id] = @user.id
+      flash[:notice] = "ユーザー「#{@user.user}」の新規登録に成功しました"
+      redirect_to root_path
     else
-      render 'new'
+      flash.now[:danger] = 'ユーザーの新規登録とログインに失敗しました'
+      render :new
     end
-  end
-
-  def edit
-    @user = User.find(params[:id])
-  end
-
-  def update
-    @user = User.find(params[:id])
-    if @user.update(user_params)
-      flash[:notice] = "タスク「#{@user.user}」を更新しました"
-      redirect_to user_path(@user.id)
-    else
-      render 'new'
-    end
-  end
-
-  def destroy
-    @user = User.find(params[:id])
-    @task.destroy
-    redirect_to users_path
-    flash[:notice] = "タスク「#{@task.name}」を更新しました"
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:user, :email, :password, :password_confirmation)
+    params.require(:user).permit(:user, :email, :admin, :password, :password_confirmation)
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless @user == current_user
   end
 end
