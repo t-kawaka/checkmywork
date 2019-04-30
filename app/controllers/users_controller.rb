047930d027aa4d12ef2b74da4db9a828c53a5592
t-kawaka <t.kawaka1136@gmail.com>
2019-04-30 05:57:10 +0000

    新規登録画面のcontroller修正（登録後にtask画面に移動するよう修正）

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,6 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = @user.id
       flash[:notice] = "ユーザー「#{@user.user}」の新規登録に成功しました"
       redirect_to root_path
     else
