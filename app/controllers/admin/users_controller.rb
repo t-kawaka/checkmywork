18d845f39dfdd8a9935d8c8361a5c64c91c900e5
t-kawaka <t.kawaka1136@gmail.com>
2019-05-01 14:57:02 +0000

    管理登録画面・一覧画面作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,50 +1,64 @@
 class Admin::UsersController < ApplicationController
+  skip_before_action :login_required, only: %i[new create]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
   def index
-    @users = User.all
+    @users = User.all.includes(:tasks).recent
   end
 
   def show
-    @user = User.find(params[:id])
+    @tasks = current_user.tasks.recent.page(params[:page])
   end
 
   def new
-    @user = User.new
+    if logged_in?
+      redirect_to root_path
+    else
+      @user = User.new
+    end
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "タスク「#{@user.user}」を作成しました"
-      redirect_to admin_user_path(@user.id)
+      redirect_to admin_users_path
     else
       render 'new'
     end
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      flash[:notice] = "タスク「#{@user.user}」を更新しました"
-      redirect_to admin_user_path(@user.id)
+      flash[:notice] = "ユーザー「#{@user.user}」を更新しました"
+      redirect_to admin_users_path
     else
-      render 'new'
+      render 'edit'
     end
   end
 
   def destroy
-    @user = User.find(params[:id])
-    @task.destroy
-    redirect_to admin_users_path
-    flash[:notice] = "タスク「#{@user.user}」を更新しました"
+    if @user.destroy
+      redirect_to admin_users_path
+      flash[:notice] = "ユーザー「#{@user.user}」を削除しました"
+    else
+      redirect_to admin_users_path
+      flash[:notice] = "ユーザー「#{@user.user}」を削除に失敗しました"
+    end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:user, :email, :password, :password_confirmation)
+    params.require(:user).permit(:user, :email, :password, :password_confirmation, :admin)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { email.downcase! }
   validates :password, presence: true, length: { minimum: 6 }
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
+  scope :recent, -> { order(created_at: :desc) }
 end
